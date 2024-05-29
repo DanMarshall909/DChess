885be9aae2c8e0291bf96bc9eaada4567b9aeda3
Dan Marshall <danmarshall909@gmail.com>
2024-05-29 11:12:16 +1000

    Write failing test to check that knights can ONLY move in an L shape

--- test/DChess.Test.Unit/Rules/KnightTests.cs
+++ test/DChess.Test.Unit/Rules/KnightTests.cs
@@ -4,41 +4,70 @@ namespace DChess.Test.Unit.Rules;
 
 public class KnightTests(BoardFixture fixture) : BoardTestBase(fixture)
 {
-    [Fact(DisplayName = "Knights can only move in an L shape")]
-    public void knights_can_only_move_in_an_L_shape()
+    private (int, int)[] _validKnightMovementOffsets =
     {
-        for (byte rank = 1; rank < 8; rank++)
+        (1, -2), (-1, 2), (1, -2), (-1, -2), (-2, 1), (2, -1), (-2, 1), (-2, -1)
+    };
+
+    [Fact(DisplayName = "Knights can only move in an L shape")]
+    public void knights_can_move_in_an_L_shape()
+    {
+        for (byte rank = 3; rank < 6; rank++)
         {
-            for (var file = 'a'; file < 'h'; file++)
+            for (var file = 'c'; file < 'f'; file++)
             {
                 var from = new Coordinate(file, rank);
 
                 Board.Clear();
                 Board[from] = WhiteKnight;
 
-                try
+                foreach (var (df, dr) in _validKnightMovementOffsets)
                 {
-                    void KnightShouldBeAbleToMoveOffsetBy(int df, int dr)
+                    KnightShouldBeAbleToMoveByOffset(df, dr);
+                }
+
+                continue;
+
+                void KnightShouldBeAbleToMoveByOffset(int df, int dr)
+                {
+                    var to = from.Offset(df, dr);
+                    Board.Pieces[from].CheckMove(to).Valid
+                        .Should().BeTrue(
+                            $"Knight should be able to move in a from {from} to {to})");
+                }
+            }
+        }
+    }
+
+    [Fact(DisplayName = "Knights can ONLY move in an L shape")]
+    public void knights_can_only_move_in_an_L_shape()
+    {
+        // check every possible starting position from a3 to f5 excluding out of bounds positions
+        for (byte fromRank = 3; fromRank < 6; fromRank++)
+        {
+            for (var fromFile = 'c'; fromFile < 'f'; fromFile++)
+            {
+                var from = new Coordinate(fromFile, fromRank);
+
+                Board.Clear();
+                Board[from] = WhiteKnight;
+
+                // check every possible move and make sure it's not invalid except for the offsets specified in _validKnightMovementOffsets
+                for (byte toRank = 1; toRank < 8; toRank++)
+                {
+                    for (var toFile = 'a'; toFile < 'h'; toFile++)
                     {
+                        int df = from.File - toFile;
+                        int dr = from.Rank - toRank;
+                        
+                        if (_validKnightMovementOffsets.Contains((df, dr)))
+                            continue;
+
                         var to = from.Offset(df, dr);
                         Board.Pieces[from].CheckMove(to).Valid
-                            .Should().BeTrue(
-                                $"Knight should be able to move from {from} to {to})");
+                            .Should().BeFalse(
+                                $"Knight should not be able to move from {from} to {to})");
                     }
-
-                    KnightShouldBeAbleToMoveOffsetBy(1, -2);
-                    KnightShouldBeAbleToMoveOffsetBy(-1, 2);
-                    KnightShouldBeAbleToMoveOffsetBy(1, -2);
-                    KnightShouldBeAbleToMoveOffsetBy(-1, 2);
-
-                    KnightShouldBeAbleToMoveOffsetBy(-2, 1);
-                    KnightShouldBeAbleToMoveOffsetBy(2, -1);
-                    KnightShouldBeAbleToMoveOffsetBy(-2, 1);
-                    KnightShouldBeAbleToMoveOffsetBy(2, -1);
-                }
-                catch (InvalidCoordinateException)
-                {
-                    // Ignore invalid coordinates for this test (handled in other tests)
                 }
             }
         }
