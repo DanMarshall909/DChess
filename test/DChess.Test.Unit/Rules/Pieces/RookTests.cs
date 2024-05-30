28bd16977028e30b4f49e7c36f3876e64cf88e45
Dan Marshall <danmarshall909@gmail.com>
2024-05-30 19:19:47 +1000

    Add rule: Rooks can move vertically

--- test/DChess.Test.Unit/Rules/Pieces/RookTests.cs
+++ test/DChess.Test.Unit/Rules/Pieces/RookTests.cs
@@ -9,4 +9,12 @@ public class RookTests(BoardFixture fixture) :BoardTestBase(fixture)
 
         Board.Pieces[a1].MoveTo(h1);
     }
+    
+    [Fact(DisplayName = "Rooks can move vertically")]
+    public void rooks_can_move_vertically()
+    {
+        Board[a1] = WhiteRook;
+
+        Board.Pieces[a1].MoveTo(a8);
+    }
 }
