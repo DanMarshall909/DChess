8462e937bfc6abb57a33579ab6f71dea6966e4ef
Dan Marshall <danmarshall909@gmail.com>
2024-05-31 16:52:34 +1000

    Add Rook tests

--- test/DChess.Test.Unit/Rules/Pieces/RookTests.cs
+++ test/DChess.Test.Unit/Rules/Pieces/RookTests.cs
@@ -1,10 +1,31 @@
 ﻿namespace DChess.Test.Unit.Rules.Pieces;
 
-public class RookTests(BoardFixture fixture) :BoardTestBase(fixture)
+public class RookTests(BoardFixture fixture) : BoardTestBase(fixture)
 {
+    private const int X = LegalPositionValue;
 
-    [Fact(DisplayName = "Rooks only can move vertically")]
-    public void rooks_can_move_vertically()
+    [Fact(DisplayName = "Rooks can move vertically")]
+    public void rooks_can_only_move_vertically()
     {
+        WhiteRook.ShouldOnlyBeAbleToMoveTo((new byte[17, 17]
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { X, X, X, X, X, X, X, X, 0, X, X, X, X, X, X, X, X },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, X, 0, 0, 0, 0, 0, 0, 0, 0 },
+        }).ToMoveOffsets());
     }
 }
