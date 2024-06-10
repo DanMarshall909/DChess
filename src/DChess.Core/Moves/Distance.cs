e5372bd1de834851d01b0cfcd503bc13f404dc32
Dan Marshall <danmarshall909@gmail.com>
2024-06-10 12:33:17 +1000

    Fix Distance.Total

--- src/DChess.Core/Moves/Distance.cs
+++ src/DChess.Core/Moves/Distance.cs
@@ -6,7 +6,7 @@ public readonly struct Distance
     {
         Vertical = Math.Abs(move.To.Rank - move.From.Rank);
         Horizontal = Math.Abs(move.To.File - move.From.File);
-        Total = (int)Math.Round(Math.Sqrt(Horizontal + Vertical));
+        Total = (int)Math.Abs(Math.Floor(Math.Sqrt(Horizontal*Horizontal + Vertical* Vertical)));
     }
 
     public int Total { get; }
